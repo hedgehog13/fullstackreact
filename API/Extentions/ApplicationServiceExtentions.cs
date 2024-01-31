b2457c38621d2e98bba0e03420d96bf5490e840c
hedgehog13 <ira.varshavsky@gmail.com>
2024-01-31 13:46:19 -0500

    CRUD functionality

---
+++ API/Extentions/ApplicationServiceExtentions.cs
@@ -0,0 +1,31 @@
+
+using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extentions
+{
+    public static class ApplicationServiceExtentions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(op => { op.UseSqlite(config.GetConnectionString("DefaultConnection")); });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+            services.AddAutoMapper(typeof(MappingProfiles));
+
+            return services;
+        }
+
+    }
+}
