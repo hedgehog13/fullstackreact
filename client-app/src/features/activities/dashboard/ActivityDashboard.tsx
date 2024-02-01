378bc7b77316d6b7358011efbe130b8da796f065
hedgehog13 <ira.varshavsky@gmail.com>
2024-01-31 19:41:41 -0500

    activitylist component, styling

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,17 @@
+
+import { Grid } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+
+interface Props {
+    activities: Activity[];
+}
+export default function ActivityDashboard({ activities }: Props) {
+    return (
+        <Grid>
+            <Grid.Column with='10'>
+               <ActivityList activities={activities}/>
+            </Grid.Column>
+        </Grid>
+    )
+}
