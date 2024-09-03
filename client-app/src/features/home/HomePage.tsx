99a0ff02e12c10af19604fae8221adbb59fe8a3d
hedgehog13 <ira.varshavsky@gmail.com>
2024-09-03 11:22:56 -0400

    updated client app for CI

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -14,7 +14,7 @@ export default observer(function HomePage() {
             <Container text>
                 <Header as='h1' inverted>
                     <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
-                    Reactivities
+                    Reactivities!!!
                 </Header>
                 {userStore.isLoggedIn ? (
                     <>
