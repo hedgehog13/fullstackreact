e3dcbb3b8d7805aabce47f9ff2cfe703186a1eea
hedgehog13 <ira.varshavsky@gmail.com>
2024-09-05 18:04:16 -0400

    echo message for the path

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
 
         public IActionResult Index()
         {
-            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "index.html"), "text/HTML");
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","index.html"), "text/HTML");
         }
     }
 }
