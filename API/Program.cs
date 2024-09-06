ecc96f4cce873bb33c0c39c66515a47b7ea74ac1
hedgehog13 <ira.varshavsky@gmail.com>
2024-09-05 21:51:12 -0400

    Security headers added

--- API/Program.cs
+++ API/Program.cs
@@ -24,11 +24,30 @@ var app = builder.Build();
 app.UseDeveloperExceptionPage();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
+    app.UseXContentTypeOptions();
+    app.UseReferrerPolicy(opt => opt.NoReferrer());
+    app.UseXXssProtection(opt => opt.EnabledWithBlockMode());//prevent cross site scripting
+    app.UseXfo(opt => opt.Deny());//prevent clickjacking (iframe)
+    app.UseCsp(opt=>opt
+        .BlockAllMixedContent()
+        .StyleSources(s=>s.Self().CustomSources("https://fonts.googleapis.com"))
+        .FontSources(s=>s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+        .FormActions(s=>s.Self())
+        .FrameAncestors(s=>s.Self())
+        .ImageSources(s=>s.Self().CustomSources("blob:", "https://res.cloudinary.com" ))
+        .ScriptSources(s=>s.Self())
+    );
 if (app.Environment.IsDevelopment())
 {
 
     app.UseSwagger();
     app.UseSwaggerUI();
+}else{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Append("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 app.UseCors("CorsPolicy");
